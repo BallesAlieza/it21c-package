680d59c3e3a7883245bcb6cd317731b1c3d03e0a
BallesAlieza <aliezaballes2@gmail.com>
2023-11-17 11:12:00 +0800

    added food package

---
+++ src/food/jobee.java
@@ -0,0 +1,12 @@
+package food;
+
+
+public class jobee {
+    public static void main (String[] args) {
+        System.out.println("food");
+    }
+    void fries(){
+        String name = null;
+        System.out.println(name + "super lami");
+    }
+}
