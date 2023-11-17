cc993e3a06511aa32a4ded19d7632d9ffc0df16f
BallesAlieza <aliezaballes2@gmail.com>
2023-11-17 10:59:17 +0800

    created hobby package

---
+++ src/hobby/Mian.java
@@ -0,0 +1,13 @@
+package hobby;
+
+
+public class Mian {
+    
+    public static void main (String[] args) {
+        System.out.println("hobby");
+    }
+    void sleep(){
+        String name = null;
+        System.out.println(name + "kay gi kapoy");
+    }
+}
---
+++ src/hobby/hobby.java
@@ -0,0 +1,9 @@
+package hobby;
+
+
+public class hobby {
+    public static void main (String[] args){
+        System.out.println("hobby");
+    }
+    
+}
